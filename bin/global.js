66c24fcedbffbce56f5688dd530e35bf0c4765a2
Yuliam-latop <yuliam.rivera@outlook.com>
2023-01-28 15:14:17 -0500

    Se agrega frases motivacionales, se agrega dependencia cowsay y lolcat, se llama la funcion para palabras motivacionales

--- bin/global.js
+++ bin/global.js
@@ -1,3 +1,12 @@
-import random from "../src/index.js"
+#! /home/yuliamr/.nvm/versions/node/v18.12.1/bin node
 
-random.motivationalCommit()
+import random from "../src/index.js"
+import cowsay from "cowsay"
+import lolcatjs from "lolcatjs"
+
+const say = cowsay.say({ text: random.motivationalCommit(), r: true })
+
+lolcatjs.options.seed = Math.round(Math.random() * 1000)
+lolcatjs.options.colors = true
+
+lolcatjs.fromString(say)
--- src/index.js
+++ src/index.js
@@ -12,10 +12,22 @@ const funnyMessages = [
   "This is not the commit message you are looking for",
   "Batman! (this commit has no parents)",
 ]
+const motivationalMessages = [
+  "Tu ezfuerzo y dedicación te pondrán al lado de los más Grandes",
+  "there is not tommorrow",
+  "Cuando tienes claro tu objetivo y sabes para donde vas, no te dejas provocar por nadie.",
+  "Perdona a tus padres: 100%, \n Perdone a los otros: 100& y \n perdónate a ti mismo: 100%.",
+  "El ejercicio cambiará tu mente, tu actitud, tu humor y tu estabilidad emocional",
+  "No pierde el que se cae, \n pierde el que no se levanta",
+  "Aprende a perder hasta ganar,",
+  "El hombre que sabe lo que quiere hacer tiene mucha suerte, \n porque ese hombre no trabajará nunca en su vida",
+  "Repite después de mi: \n nunca permitiré que nadie me haga sentir que \n no soy suficiente",
+]
 
 const motivationalCommit = () => {
-  const message = funnyMessages[Math.floor(Math.random() * funnyMessages.length)]
-  console.log(`\x1b[34m${message}\x1b[89m`)
+  const message = motivationalMessages[Math.floor(Math.random() * motivationalMessages.length)]
+  // console.log(`\x1b[34m${message}\x1b[89m`)
+  return message
 }
 
 export default {
