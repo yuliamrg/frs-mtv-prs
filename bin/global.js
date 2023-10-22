eaa6e164c0e9100827c4738f635a496e6a49950a
Yuliam-latop <yuliam.rivera@outlook.com>
2023-10-22 16:48:46 -0500

    :sparkles: Se añade funcion global

---
+++ bin/global.js
@@ -0,0 +1,24 @@
+#! /home/yuliamr/.nvm/versions/node/v18.12.1/bin/node
+
+import { motivationalCommit } from "../src/index.js"
+import { funnyCommit } from "../src/index.js"
+import cowsay from "cowsay"
+import lolcatjs from "lolcatjs"
+
+let option = process.argv[3]
+let commit
+
+if (option == '-m') {
+  commit = motivationalCommit()
+} else if (option == '-f') {
+  commit = funnyCommit()
+} else {
+  commit = motivationalCommit()
+}
+
+const say = cowsay.say({ text: commit, r: true })
+lolcatjs.options.seed = Math.round(Math.random() * 1000)
+lolcatjs.options.colors = true
+
+lolcatjs.fromString(say)
+
